d58a643a7e504494ba089ac3e5e03d475273901e
Lukich <rr3ostat@ya.ru>
2021-01-12 14:30:59 +0300

    Сайт доделан до рабочего состояния, добавлены статьи.

--- PycharmProjects/site/app.py
+++ PycharmProjects/site/app.py
@@ -24,5 +24,30 @@ def galery():
     return render_template("galery.html")
 
 
+@app.route("/st")
+def st():
+    return render_template('st.html')
+
+
+@app.route("/1")
+def s1():
+    return render_template('1.html')
+
+
+@app.route("/2")
+def s2():
+    return render_template('2.html')
+
+
+@app.route("/3")
+def s3():
+    return render_template('3.html')
+
+
+@app.route("/4")
+def s4():
+    return render_template('4.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
